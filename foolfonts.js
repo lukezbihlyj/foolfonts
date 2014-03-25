a3a8decb16655cdc0e119066d3116fa9daa93964
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2014-03-25 23:09:00 +0000

    Initial build of foolfonts.

---
+++ foolfonts.js
@@ -0,0 +1,90 @@
+/**
+ * Simple library for setting the font of every element on a page to Comic
+ * Sans with minimal effort.
+ */
+
+var foolfonts = {
+    /**
+     * An array of font names to ignore, which will cause the parser to not
+     * make them Comic Sans MS. This is useful for icon fonts and logo fonts.
+     *
+     * @var {array}
+     */
+
+    _ignore: [],
+
+    /**
+     * Initiate the horrible, horrible process of iterating through each
+     * element in the page and checking if it's renderable and doesn't
+     * contain an already-ignored font.
+     */
+
+    init: function(options) {
+    	if(!options.force && !this.is_april_1st()) {
+    		return;
+    	}
+
+    	this.ignore("Comic Sans MS");
+
+        if(options.ignore && typeof options.ignore === "string") {
+            this.ignore(options.ignore);
+        } else if(options.ignore && typeof options.ignore === "array") {
+            for(var i in options.ignore) {
+                this.ignore(options.ignore[i]);
+            }
+        }
+
+        var elements = document.documentElement.getElementsByTagName("*");
+
+        for(var i in elements) {
+            var element = elements[i];
+            var computed = window.getComputedStyle(element) || element.currentStyle;
+
+            if(computed !== null && typeof computed !== "undefined") {
+                var font = "fontFamily" in computed && computed.fontFamily;
+
+                if(font !== null && typeof font === "string") {
+                    this.check(element, font);
+                }
+            }
+        }
+    },
+
+    /**
+     * Check if the element has a font that is on the ignore list, which
+     * conveniently includes our beautiful Comic Sans MS font itself.
+     *
+     * @param {object} element The element to check.
+     * @param {string} font The currently applied font names.
+     */
+
+    check: function(element, font) {
+    	var lowercase = font.toLowerCase();
+
+    	for(var j in this._ignore) {
+        	if(lowercase.indexOf(this._ignore[j]) !== -1) return;
+        }
+
+        element.style.fontFamily = "'Comic Sans MS', " + font;
+    },
+
+    /**
+     * Check if it currently April 1st, otherwise we really shouldn't be
+     * destroying these websites...
+     */
+
+    is_april_1st: function() {
+    	return new Date().getMonth() == 4 && new Date().getDate() == 1;
+    },
+
+    /**
+     * Ignore the specified font, which we cast to lower case for easier
+     * comparison later
+     *
+     * @param {string} font_name The font name to ignore.
+     */
+
+    ignore: function(font_name) {
+        this._ignore.push(font_name.toLowerCase());
+    }
+};
